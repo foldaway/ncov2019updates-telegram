3eb03b109dc5d092d90a60a3f50ab4ff4af1e63c
Duncan Leo <hello@duncanleo.me>
2020-02-03 22:03:59 +0800

    chore: working bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('test');
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import Telegraf, {ContextMessageUpdate} from 'telegraf';
+
+const { TELEGRAM_BOT_TOKEN } = process.env;
+
+if (!TELEGRAM_BOT_TOKEN) {
+  throw new Error('Missing bot token');
+}
+
+const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
+
+bot.start((ctx: ContextMessageUpdate) => ctx.reply('wtf'));
+bot.launch();
