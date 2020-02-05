6152a9d890b36a83a64beb0fc497196ec8f684d8
Duncan Leo <hello@duncanleo.me>
2020-02-05 23:56:53 +0800

    feat: add /subscribe

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as dotenv from 'dotenv';
 import { createHandyClient } from 'handy-redis';
+import { Region, Subscription } from './db';
 dotenv.config();
 
 import Telegraf, {
@@ -24,36 +25,88 @@ const bot: Telegraf<SceneContextMessageUpdate> = new Telegraf(
   TELEGRAM_BOT_TOKEN
 );
 
+// Scenes
+
 const statusScene = new BaseScene('status');
 statusScene.on('message', async ctx => {
+  const regionName = ctx.message?.text;
+  const region = await Region.findOne({
+    where: { name: regionName || '' },
+  });
+  if (!region) {
+    ctx.reply('Region not found!');
+    await ctx.scene.leave();
+    return;
+  }
+
   const data = await redisClient.hgetall(`BNO.${ctx.message?.text}`);
   await ctx.reply(
     Object.keys(data)
       .map(key => `*${key}:* ${data[key]}`)
       .join('\n'),
-    { parse_mode: 'MarkdownV2' }
+    { parse_mode: 'Markdown' }
   );
   await ctx.scene.leave();
 });
 
-const stage = new Stage([statusScene]);
+const subscribeScene = new BaseScene('subscribe');
+subscribeScene.on('message', async ctx => {
+  const regionName = ctx.message?.text;
+  const region: Region = await Region.findOne({
+    where: { name: regionName || '' },
+  });
+  if (!region) {
+    ctx.reply('Region not found!');
+    await ctx.scene.leave();
+    return;
+  }
+
+  const subscription: Subscription = await Subscription.findOrCreate({
+    where: {
+      chatId: (await ctx.getChat()).id,
+      region_id: region.id,
+    },
+  });
+
+  if (!subscription) {
+    ctx.reply('Error subscribing');
+    return;
+  }
+
+  ctx.reply(`Subscribed to '${regionName}'`);
+  await ctx.scene.leave();
+});
+
+const stage = new Stage([statusScene, subscribeScene]);
 stage.command('cancel', Stage.leave());
 
 bot.use(session());
 bot.use(stage.middleware());
 
-bot.start((ctx: ContextMessageUpdate) => ctx.reply('wtf'));
-bot.command('status', async ctx => {
-  const regions = await redisClient.lrange('REGIONS', 0, -1);
+// Message handlers
+async function displayRegionsKeyboard(
+  ctx: ContextMessageUpdate
+): Promise<void> {
+  const regions = await Region.findAll();
 
   ctx.reply('Choose a region', {
     reply_markup: {
       one_time_keyboard: true,
-      keyboard: regions.map(region => [region]),
+      keyboard: regions.map((region: Region) => [region.name]),
     },
   });
+}
 
+bot.start((ctx: ContextMessageUpdate) => ctx.reply('wtf'));
+
+bot.command('status', async ctx => {
+  await displayRegionsKeyboard(ctx);
   ctx.scene.enter('status');
+});
+
+bot.command('subscribe', async ctx => {
+  await displayRegionsKeyboard(ctx);
+  ctx.scene.enter('subscribe');
 });
 
 bot.launch();
